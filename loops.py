56d289316e0dac408006223cfe5d6e0060d45a85
DillonGrabskiInquiries <sahasrahlainquiries@gmail.com>
2021-06-07 14:56:44 -0500

    yep

---
+++ loops.py
@@ -0,0 +1,37 @@
+# for x in range(151):
+#     print(x)
+
+
+# count = 5
+# while count < 1005:
+#     print(count)
+#     count += 5
+
+
+# for x in range(1, 101):
+#     if x % 5 == 0:
+#         print("Coding")
+#     if x % 10 == 0:
+#         print("Coding Dojo")
+#     else:
+#         print(x)
+#     x += 1
+
+
+# for x in range(0, 500000):
+#     if x % 2 != 0:
+#         print(x)
+
+
+# count = 2018
+# while count > 0:
+#     print(count)
+#     count -= 4
+
+
+# lowNum = 0
+# highNum = 4000
+# mult = 6
+# for x in range(lowNum, highNum):
+#     if x % mult == 0:
+#         print(x)
